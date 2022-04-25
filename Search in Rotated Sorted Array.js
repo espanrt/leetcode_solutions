f76dfdab0b73871652dbd8aa40ff7617c927ecf2
alejandro vidal <102489433+espanrt@users.noreply.github.com>
2022-04-24 21:42:02 -0500

    Create Search in Rotated Sorted Array.js

---
+++ Search in Rotated Sorted Array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    
+    let j=nums.indexOf(target)
+    return j
+    
+};
